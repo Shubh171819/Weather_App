f017ab25695dbd23d207bb9449733f28e800bc40
Shubh171819 <113910175+Shubh171819@users.noreply.github.com>
2024-02-24 00:01:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+const search = document.querySelector("#search");
+const weather = document.querySelector("#weather");
+
+const API_KEY = `962d251ec8af84497efb0828366a1d19`;
+
+const getWeather = async (city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    showWeather(data);
+    search.value = "";
+}
+
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
+    weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    getWeather(search.value)
+}
+)
